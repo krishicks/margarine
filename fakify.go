6dedab2cd106f84b635ac655c94de30fb5f8f05a
Kris Hicks <khicks@pivotal.io>
2016-11-29 11:49:50 -0800

    stub func on struct, mutex for func

--- fakify.go
+++ fakify.go
@@ -6,6 +6,8 @@ import (
 	"go/ast"
 	"go/format"
 	"go/token"
+	"unicode"
+	"unicode/utf8"
 
 	"golang.org/x/tools/imports"
 )
@@ -15,99 +17,68 @@ type FakifyOpts struct {
 }
 
 func Fakify(file *ast.File, opts FakifyOpts) *ast.File {
-	// add stubs to struct
+	// find struct
 	var structType *ast.StructType
+	var obj *ast.Object
 	ast.Inspect(file, func(node ast.Node) bool {
 		if node == nil {
 			return true
 		}
 
-		var obj *ast.Object
-		switch v := node.(type) {
-		case *ast.TypeSpec:
-			if v.Name != nil && v.Name.Name != opts.StructName {
-				return true
-			}
+		v, ok := node.(*ast.TypeSpec)
+		if !ok {
+			return true
+		}
 
-			var ok bool
-			structType, ok = v.Type.(*ast.StructType)
-			if !ok {
-				return true
-			}
+		if v.Name != nil && v.Name.Name != opts.StructName {
+			return true
+		}
 
-			obj = v.Name.Obj
-		case *ast.FuncDecl:
-			if structType == nil {
-				break
-			}
+		structType, ok = v.Type.(*ast.StructType)
+		if !ok {
+			return true
+		}
 
-			if obj != nil { // hmm
-				if expr, ok := v.Recv.List[0].Type.(*ast.StarExpr); ok {
-					if ident, ok := expr.X.(*ast.Ident); ok {
-						if ident.Obj != obj {
-							// this isn't the object you're looking for
-							break
-						}
+		obj = v.Name.Obj
+
+		return false
+	})
+
+	if structType == nil {
+		panic("could not find struct")
+	}
+
+	if obj == nil {
+		panic("no obj for typespec")
+	}
+
+	// find methods for struct
+	var funcDecls []*ast.FuncDecl
+	ast.Inspect(file, func(node ast.Node) bool {
+		if v, ok := node.(*ast.FuncDecl); ok {
+			switch expr := v.Recv.List[0].Type.(type) {
+			//TODO: support non-pointer receiver
+			case *ast.StarExpr:
+				if ident, ok := expr.X.(*ast.Ident); ok {
+					if ident.Obj == obj {
+						funcDecls = append(funcDecls, v)
 					}
 				}
 			}
-
-			params := &ast.FieldList{}
-			for _, field := range v.Type.Params.List {
-				var i int
-				if len(field.Names) > 0 {
-					// this path isn't hit if Structify is used; it removes multi-named fields
-					for range field.Names {
-						i++
-						params.List = append(params.List, &ast.Field{
-							Type: field.Type,
-						})
-					}
-				} else {
-					i++
-					params.List = append(params.List, &ast.Field{
-						Type: field.Type,
-					})
-				}
-			}
-
-			results := &ast.FieldList{}
-			if v.Type.Results != nil {
-				for _, field := range v.Type.Results.List {
-					var i int
-					if len(field.Names) > 0 {
-						// this path isn't hit if Structify is used; it removes named fields
-						for range field.Names {
-							i++
-							results.List = append(results.List, &ast.Field{
-								Type: field.Type,
-							})
-						}
-					} else {
-						i++
-						results.List = append(results.List, &ast.Field{
-							Type: field.Type,
-						})
-					}
-				}
-			}
-
-			structType.Fields.List = append(structType.Fields.List, &ast.Field{
-				Names: []*ast.Ident{
-					ast.NewIdent(v.Name.Name + "Stub"), // missing Obj; necessary? would include Kind: var
-				},
-				Type: &ast.FuncType{
-					Params:  params,
-					Results: results,
-				},
-			})
-		default:
-			// fmt.Printf("%p -> %#v\n", v, v)
 		}
 
 		return true
 	})
 
+	for _, funcDecl := range funcDecls {
+		stubFuncOnStruct(structType, funcDecl)
+
+		privateName := privatize(funcDecl.Name.Name)
+		addMutexForFuncOnStruct(structType, privateName)
+	}
+
+	//   for each found method
+	//      add stubs to struct
 	fset := token.NewFileSet()
 	var buf bytes.Buffer
 	err := format.Node(&buf, fset, file)
@@ -125,5 +96,72 @@ func Fakify(file *ast.File, opts FakifyOpts) *ast.File {
 	return nil
 }
 
-func fakeMethod() {
+func privatize(s string) string {
+	if s == "" {
+		return ""
+	}
+	r, n := utf8.DecodeRuneInString(s)
+	return string(unicode.ToLower(r)) + s[n:]
+}
+
+func addMutexForFuncOnStruct(structType *ast.StructType, privateName string) {
+	structType.Fields.List = append(structType.Fields.List, &ast.Field{
+		Type: &ast.SelectorExpr{
+			X:   ast.NewIdent("sync"),
+			Sel: ast.NewIdent("RWMutex"),
+		},
+		Names: []*ast.Ident{ast.NewIdent(privateName + "Mutex")},
+	})
+}
+
+func stubFuncOnStruct(structType *ast.StructType, funcDecl *ast.FuncDecl) {
+	params := &ast.FieldList{}
+	for _, field := range funcDecl.Type.Params.List {
+		var i int
+		if len(field.Names) > 0 {
+			// this path isn't hit if Structify is used; it removes multi-named fields
+			for range field.Names {
+				i++
+				params.List = append(params.List, &ast.Field{
+					Type: field.Type,
+				})
+			}
+		} else {
+			i++
+			params.List = append(params.List, &ast.Field{
+				Type: field.Type,
+			})
+		}
+	}
+
+	results := &ast.FieldList{}
+	if funcDecl.Type.Results != nil {
+		for _, field := range funcDecl.Type.Results.List {
+			var i int
+			if len(field.Names) > 0 {
+				// this path isn't hit if Structify is used; it removes named fields
+				for range field.Names {
+					i++
+					results.List = append(results.List, &ast.Field{
+						Type: field.Type,
+					})
+				}
+			} else {
+				i++
+				results.List = append(results.List, &ast.Field{
+					Type: field.Type,
+				})
+			}
+		}
+	}
+
+	structType.Fields.List = append(structType.Fields.List, &ast.Field{
+		Names: []*ast.Ident{
+			ast.NewIdent(funcDecl.Name.Name + "Stub"), // missing Obj; necessary? would include Kind: var
+		},
+		Type: &ast.FuncType{
+			Params:  params,
+			Results: results,
+		},
+	})
 }
